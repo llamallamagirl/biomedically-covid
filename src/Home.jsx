d22d9e283c64b7a42c37d3314d552cb2c856caa7
Kristin Lindquist <lindquist.kristin@gmail.com>
2020-03-24 12:51:57 -0600

    sortable table

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 // @flow
 
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { Container, Table } from "react-bootstrap";
 
 import FilterBar from "./components/FilterBar";
@@ -8,8 +9,22 @@ import Header from "./components/Header";
 import Intervention from "./components/Intervention";
 import { interventions } from "./data";
 
+const sortTable = (a, b, sort) => {
+  const aEt = (a.evidenceTypes[sort] || {}).evidence || [];
+  const bEt = (b.evidenceTypes[sort] || {}).evidence || [];
+  return !sort || sort === "name" || !aEt
+    ? a.name.localeCompare(b.name)
+    : aEt.length - bEt.length;
+};
+
+const useQuery = () => new URLSearchParams(useLocation().search);
+
 const Home = ({ history, match }) => {
+  const query = useQuery();
   const ccId = parseInt(match.params.ccId);
+  const sort = query.get("sort");
+  const reverse = query.get("reverse") === "true";
+
   return (
     <Container style={{ minHeight: "1000px" }}>
       <Header />
@@ -17,16 +32,53 @@ const Home = ({ history, match }) => {
       <Table striped bordered hover>
         <thead>
           <tr>
-            <th>Treatment</th>
-            <th className="text-center">MoA</th>
-            <th className="text-center text-nowrap">In Vitro</th>
-            <th className="text-center text-nowrap">In Vivo</th>
-            <th className="text-center">Human</th>
+            <th
+              onClick={() =>
+                history.push(`/${ccId}?sort=name&reverse=${!reverse}`)
+              }
+            >
+              Treatment
+            </th>
+            <th
+              className="text-center"
+              onClick={() =>
+                history.push(`/${ccId}?sort=MoA&reverse=${!reverse}`)
+              }
+            >
+              MoA
+            </th>
+            <th
+              className="text-center text-nowrap"
+              onClick={() =>
+                history.push(`/${ccId}?sort=InVitro&reverse=${!reverse}`)
+              }
+            >
+              In Vitro
+            </th>
+            <th
+              className="text-center text-nowrap"
+              onClick={() =>
+                history.push(`/${ccId}?sort=InVivo&reverse=${!reverse}`)
+              }
+            >
+              In Vivo
+            </th>
+            <th
+              className="text-center"
+              onClick={() =>
+                history.push(`/${ccId}?sort=Human&reverse=${!reverse}`)
+              }
+            >
+              Human
+            </th>
           </tr>
         </thead>
         <tbody>
           {interventions
             .filter((i) => i.stages.includes(ccId) || ccId === 0)
+            .sort((a, b) =>
+              reverse ? sortTable(b, a, sort) : sortTable(a, b, sort)
+            )
             .map((i) => (
               <Intervention key={i.id} {...i} />
             ))}
